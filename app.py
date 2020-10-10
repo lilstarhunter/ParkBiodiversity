b1d5d82b0e5e4d16a51053d35131e128f348a439
codereber <jamesmpyle@gmail.com>
2020-10-10 08:11:26 -0400

    update

---
+++ app.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify, render_template
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///new.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Survive = Base.classes.survive
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return render_template('index.html')
+
+
+# @app.route("/api/v1.0/names")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+
+@app.route("/api/v1.0/survivors")
+def survivors():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # data = engine.execute("SELECT * FROM survive")
+
+    # """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    survives = session.query(Survive.survival, Survive.organ).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_survivors = []
+    for survival, organ in survives:
+        s_dict = {}
+        s_dict["survival"] = survival
+        s_dict["organ"] = organ
+        all_survivors.append(s_dict)
+       
+    # for record in data:
+    #     print(record)
+    return jsonify(all_survivors)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ plot.js
@@ -0,0 +1,23 @@
+d3.json("http://127.0.0.1:5000/api/v1.0/survivors").then((data) => {
+  //  Create the Traces
+  var trace1 = {
+    x: data.organ,
+    y: data.survival.map(val => Math.sqrt(val)),
+    type: "box",
+    name: "Cancer Survival",
+    boxpoints: "all"
+  };
+
+  // Create the data array for the plot
+  var data = [trace1];
+localStorage
+  // Define the plot layout
+  var layout = {
+    title: "Square Root of Cancer Survival by Organ",
+    xaxis: { title: "Organ" },
+    yaxis: { title: "Square Root of Survival" }
+  };
+
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("plot", data, layout);
+});
